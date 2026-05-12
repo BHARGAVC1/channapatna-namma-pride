18b686145366bd85032fb48e645e826b63201c4d
Bhargav C <bhargav27love@gmail.com>
2026-05-12 10:18:24 +0530

    Initial commit

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_channapatna_nammapride_ChannapatnaApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.channapatna.nammapride.ChannapatnaApp"
-)
-public final class _com_channapatna_nammapride_ChannapatnaApp {
-}
